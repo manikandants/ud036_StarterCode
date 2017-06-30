c18a42250ae4e76a0ab6e0535673467b180958d2
manikandanxenon <manikandan@wearexenon.com>
2017-06-30 22:58:29 +0530

    movie class

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ud036_StarterCode
+# Udacity Fullstack developer Nanodegree ud036
 Source code for a Movie Trailer website.
---
+++ entertainment_center.py
@@ -0,0 +1,51 @@
+# import media for Movie class
+import media
+
+# import fresh_tomatoes to generate and open web page
+import fresh_tomatoes
+
+# Create Ratatouille object
+ratatouille = media.Movie(
+    "Ratatouille",
+    "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+    "https://www.youtube.com/watch?v=c3sBBRxDAqk"
+)
+
+# Create Avatar object
+avatar = media.Movie(
+    "Avatar",
+    "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+    "https://www.youtube.com/watch?v=cRdxXPV9GNQ"
+)
+
+# Create Inside Out object
+insideOut = media.Movie(
+    "Inside Out",
+    "https://upload.wikimedia.org/wikipedia/en/0/0a/Inside_Out_%282015_film%29_poster.jpg",
+    "https://www.youtube.com/watch?v=_MC3XuMvsDI"
+)
+
+# Create Up object
+up = media.Movie(
+    "Up",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Up_%282009_film%29.jpg/220px-Up_%282009_film%29.jpg",
+    "https://www.youtube.com/watch?v=pkqzFUhGPJg"
+)
+
+# Create Up object
+wallE = media.Movie(
+    "Wall-E",
+    "https://upload.wikimedia.org/wikipedia/en/c/c2/WALL-Eposter.jpg",
+    "https://www.youtube.com/watch?v=alIq_wG9FNk"
+)
+
+# Create Up object
+toyStory = media.Movie(
+    "Toy Story",
+    "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+    "https://www.youtube.com/watch?v=KYz2wyBy3kc"
+)
+
+movies = [ratatouille, avatar, insideOut, up, wallE, toyStory]
+
+fresh_tomatoes.open_movies_page(movies)
